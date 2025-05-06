3b02c0a0276c66ef9ebb3f822e81bd55d10a8fbf
Adrià Sánchez Calvo <adria.sanchez.c@gmail.com>
2025-05-06 18:08:14 +0200

    feat: Seeders de usuarios en firestore

--- app/seeders/user_seeder.py
+++ app/seeders/user_seeder.py
@@ -2,23 +2,37 @@ from faker import Faker
 from models.user import User
 from services.user_service import add_user
 import random
+from utils.firestore_utils import add_firestore_user
 
 fake = Faker()
 
 def seed_users(users=10):
     """Genera usuarios ficticios y los inserta en la base de datos."""
     for _ in range(users):
+        phone = f"{fake.random_int(min=600000000, max=699999999)}"
+        photoUrl = get_random_image()
+        uuid = fake.uuid4()
+        username = fake.user_name()
+        
+        user_data = {
+            "uid": uuid,
+            "phone": phone,
+            "photoUrl": photoUrl,
+            "username": username,
+        }
+        
         user = User(
-            uid=fake.uuid4(),
+            uid=uuid,
             email=fake.email(),
             full_name=fake.name(),
-            username=fake.user_name(),
+            username=username,
             birth_date=fake.date_of_birth(minimum_age=18, maximum_age=80),
-            profile_picture=get_random_image(),
-            phone=fake.phone_number()
+            profile_picture=photoUrl,
+            phone=phone
         )
         add_user(user)
-    
+        add_firestore_user(user_data)
+        
     print(f"Se han insertado {users} usuarios ficticios en la base de datos.")
     
 def get_random_image(style="personas", size=200, format="png"):
---
+++ app/utils/firestore_utils.py
@@ -0,0 +1,17 @@
+from config import FIREBASECONFIG
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("app/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+userCollection = db.collection("users")
+
+def add_firestore_user(user_data):
+    """Agrega un usuario a Firestore."""
+    try:
+        userCollection.add(user_data)
+        print("Usuario agregado a Firestore.")
+    except Exception as e:
+        print(f"Error al agregar el usuario a Firestore: {e}")
